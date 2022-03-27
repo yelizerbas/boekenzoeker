37f228c150c8a9a39a47e23dbd796dfeb725737b
yelizerbas <yeliz.erbas@hotmail.com>
2022-03-27 12:28:27 +0200

    :white_check_mark: 1/2 filterfunctie werkt

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable no-undef */
 
 require('dotenv').config()
@@ -12,11 +11,14 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const flash = require('express-flash');
+const multer = require('multer');
+const {utilsDB}  = require('./utils/db')
 
 
 const funct = require('./functions.js');
 
 const app = express();
+const upload = multer();
 
 // MongoDB connectie informatie
 
@@ -24,123 +26,32 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongodbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.weqjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
-// PASSPORT
-
-// Passport sessie.
-passport.serializeUser( (user, done) => {
-  console.log("serializing " + user.username);
-  done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-  console.log("deserializing " + obj);
-  done(null, obj);
-});
-
-
-// Gebruik de LocalStrategy in Passport om gebruikers in te loggen/ te registreren.
-passport.use('local-signin', new LocalStrategy(
-  {passReqToCallback : true}, //Request naar Callback
-  (req, username, password, done) => {
-    funct.localAuth(username, password)
-    .then( (user) => {
-      if (user) {
-        console.log("LOGGED IN AS: " + user.username);
-        req.session.success = 'Je bent succesvol ingelogd ' + user.username + '!';
-        done(null, user);
-      }
-      if (!user) {
-        console.log("COULD NOT LOG IN");
-        req.session.error = 'Kon gebruiker niet inloggen.'; //iGebruiker word geinformeerd dat hij/zij niet kan inloggen
-        return done(null, false, {message: 'Verkeerde gebruikersnaam of wachtwoord'})
-      }
-    })
-    .fail((err) => {
-      console.log(err.body);
-    });
-  }
-));
-
-// Gebruik de LocalStrategy in Passport om gebruikers in te loggen/ te registreren.
-passport.use('local-signup', new LocalStrategy(
-  {passReqToCallback : true}, //Request naar Callback
-  (req, username, password, done) => {
-    funct.localReg(username, password)
-    .then( (user) => {
-      if (user) {
-        console.log("REGISTERED: " + user.username);
-        req.session.success = 'Je bent succesvol geregistreerd en ingelogt. ' + user.username + '!';
-        done(null, user);
-      }
-      if (!user) {
-        console.log("COULD NOT REGISTER");
-        req.session.error = 'Gebruikersnaam al in gebruik kies een andere en probeer opnieuw'; //
-        return done(null, false, {message: 'Gebruikersnaam bestaat al'})
-      }
-    })
-    .fail( (err) => {
-      console.log(err.body);
-    });
-  }
-));
-
 // EXPRESS
 // Express Configureren
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(upload.array()); 
 app.use(flash())
 app.use(methodOverride('_method'));
 app.use(session({secret: 'process.env.SESSION_SECRET', saveUninitialized: true, resave: true}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// // Session-persisted message middleware
-app.use( (req, res, next) => {
-  const err = req.session.error,
-      msg = req.session.notice,
-      success = req.session.success;
-
-      delete req.session.error;
-      delete req.session.success;
-      delete req.session.notice;
-
-      if (err) res.locals.error = err;
-      if (msg) res.locals.notice = msg;
-      if (success) res.locals.success = success;
-
-      next();
-});
-
-app.use((req, res, next) => {
-  app.locals.success = req.flash('success')
-  next();
-});
-
-// Handlebars instellen
-app.engine('hbs', engine({
-  layoutsDir: `${__dirname}/views/layouts/`,
-  extname: 'hbs',
-  defaultLayout: 'index',
-  partialsDir: `${__dirname}/views/partials`
-
-  }));
-
-app.set('view engine', 'hbs');
+utilsDB(client).then(data => { console.log(data)})
 
 //===============ROUTES===============
 
 //Homepagina laten zien
-app.get('/', (req, res) => {
+app.get('/account', (req, res) => {
   if ( !req.isAuthenticated() ) {
     res.redirect('/login')
     return
   }
-  res.render('main', {user: req.user});
+  res.render('account', {user: req.user});
 });
 
 //Log-in pagina laten zien
@@ -199,6 +110,129 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
   req.session.notice = "Succesvol uitgelogd " + name + "!";
 });
+
+//FILTEREN
+app.get("/", async(req, res) => {
+
+  // Data uit de database wat in een array is gestopt wordt nu in de constante "boeken"gezet
+  const boeken = await utilsDB(client); 
+  // Ophalen boeken database
+  res.render("main", {
+    boeken: boeken
+  });
+});
+
+
+app.post("/formulier", async(req, res) => {
+
+  const boeken = await utilsDB(client); 
+
+  console.log(req.body);
+  // Filter boeken
+  const filteredBoeken = boeken.filter((boeken) => {
+    // Stop het item alleen in de array wanneer onderstaande regel 'true' is, dus als de doelen overeen komen met de radiobutton
+    return boeken.genre == req.body.genre;
+  });
+  //render zelfde pagina, maar met de gefilterde boeken
+  res.render("main", {
+    boeken: filteredBoeken
+  });
+});
+
+
+
+// PASSPORT
+// Passport sessie.
+passport.serializeUser( (user, done) => {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
+
+
+// Gebruik de LocalStrategy in Passport om gebruikers in te loggen/ te registreren.
+passport.use('local-signin', new LocalStrategy(
+  {passReqToCallback : true}, //Request naar Callback
+  (req, username, password, done) => {
+    funct.localAuth(username, password)
+    .then( (user) => {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.username);
+        req.session.success = 'Je bent succesvol ingelogd ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Kon gebruiker niet inloggen.'; //iGebruiker word geinformeerd dat hij/zij niet kan inloggen
+        return done(null, false, {message: 'Verkeerde gebruikersnaam of wachtwoord'})
+      }
+    })
+    .fail((err) => {
+      console.log(err.body);
+    });
+  }
+));
+
+// Gebruik de LocalStrategy in Passport om gebruikers in te loggen/ te registreren.
+passport.use('local-signup', new LocalStrategy(
+  {passReqToCallback : true}, //Request naar Callback
+  (req, username, password, done) => {
+    funct.localReg(username, password)
+    .then( (user) => {
+      if (user) {
+        console.log("REGISTERED: " + user.username);
+        req.session.success = 'Je bent succesvol geregistreerd en ingelogt. ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT REGISTER");
+        req.session.error = 'Gebruikersnaam al in gebruik kies een andere en probeer opnieuw'; //
+        return done(null, false, {message: 'Gebruikersnaam bestaat al'})
+      }
+    })
+    .fail( (err) => {
+      console.log(err.body);
+    });
+  }
+));
+
+
+// // Session-persisted message middleware
+app.use( (req, res, next) => {
+  const err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+      delete req.session.error;
+      delete req.session.success;
+      delete req.session.notice;
+
+      if (err) res.locals.error = err;
+      if (msg) res.locals.notice = msg;
+      if (success) res.locals.success = success;
+
+      next();
+});
+
+app.use((req, res, next) => {
+  app.locals.success = req.flash('success')
+  next();
+});
+
+// Handlebars instellen
+app.engine('hbs', engine({
+  layoutsDir: `${__dirname}/views/layouts/`,
+  extname: 'hbs',
+  defaultLayout: 'index',
+  partialsDir: `${__dirname}/views/partials`
+
+  }));
+
+app.set('view engine', 'hbs');
 
 //error handling
 
