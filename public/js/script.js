b894e86d99d4372a31c73a957f1268a9a32570ad
Inevdhoven <inevdhoven@gmail.com>
2022-03-27 15:46:53 +0200

    :sparkles: Het geven van error wanneer fout ingevuld toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -25,3 +25,46 @@ async function getApi(url) {
 }
 // Calling that async function
 getApi(apiUrl);
+
+// Fout meldingen
+// Account
+const formAccount = document.querySelector('#formaccount');
+
+if (formAccount) {
+    formAccount.addEventListener('submit', (event) => {
+        const accountName = document.querySelector('#formaccount #username');
+        const accountPass = document.querySelector('#formaccount #password');
+        const errorMessageName = document.querySelector('.messagename');
+        const errorMessagePass = document.querySelector('.messagepassword');
+
+        if (accountName.value.length != 0 && isEmail(accountEmail.value) && accountPass.value.length != 0 && accountPass.value != 0) {
+            console.log('Het werkt denk ik');
+        } else {
+            event.preventDefault();
+
+            if (accountName.value.length != 0) {
+                console.log('Er is een gebruikersnaam ingevuld.');
+                errorMessageName.classList.remove('errormessage');
+                accountName.classList.remove('error');
+            } else {
+                console.log('Er is geen gebruikersnaam ingevuld.');
+                accountName.classList.add('error');
+                errorMessageName.classList.add('errormessage');
+                errorMessageName.innerHTML = 'Er is geen gebruikersnaam ingevuld.';
+            }
+
+            if (accountPass.value.length != 0) {
+                console.log('Het wachtwoord is goed');
+                accountPass.classList.remove('error');
+                accountConfirmPass.classList.remove('error');
+                errorMessagePass.classList.remove('errormessage');
+            } else {
+                console.log('Het wachtwoord mag niet leeg zijn');
+                errorMessagePass.classList.add('errormessage');
+                errorMessagePass.innerHTML = 'Er is geen wachtwoord ingevuld.';
+                accountPass.classList.add('error');
+                accountConfirmPass.classList.add('error');
+            }
+        }
+    });
+}
