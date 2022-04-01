a7c2ac6bb90570ac94254c205689da905a2d20fe
Inevdhoven <inevdhoven@gmail.com>
2022-04-01 15:52:14 +0200

    Reset

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,13 @@
-const { Book } = require('./models/User');
-
-
 exports.utilsDB = async function (client) {
     try {
-        // await client.connect()
+        await client.connect()
   
-        // const database = client.db('Accounts')
+        const database = client.db('Accounts')
   
-        // const collection = database.collection('Boeken')
+        const collection = database.collection('Boeken')
   
         // Stopt de data uit mijn database in een Array
-        return Book.find().toArray()
+        return collection.find().toArray()
   
         // Error, als de database niet doet
     } catch (err) {
