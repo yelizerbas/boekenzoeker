7ec8678ba1db42e3bfa74cace0b072255186f43f
Inevdhoven <inevdhoven@gmail.com>
2022-04-01 12:12:29 +0200

    :sparkles: Error enhancement toegevoegd

--- public/js/script.js
+++ public/js/script.js
@@ -17,49 +17,206 @@ async function getApi(url) {
     console.log(data);
     if (response) {
         const bookTip = document.querySelector('header p span:first-of-type');
-        const titleBook = data.results.lists[17].books[getRandomInt(10)].title;
+        const titleBook = data.results.lists[10].books[getRandomInt(10)].title;
         bookTip.innerHTML = titleBook;
         
-        console.log(data.results.lists[17].books[getRandomInt(10)].title);
+        console.log(data.results.lists[10].books[getRandomInt(10)].title);
     }
 }
 // Calling that async function
 getApi(apiUrl);
 
 // Fout meldingen
+// Of email correct is of niet
+// Source https://codepen.io/FlorinPop17/pen/OJJKQeK
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// Signup
+const formSignup = document.querySelector('#formsignup');
+const errorMessageName = document.querySelector('.messagename');
+const errorMessageEmail = document.querySelector('.messageemail');
+const errorMessagePass = document.querySelector('.messagepassword');
+
+if (formSignup) {
+    formSignup.addEventListener('submit', (event) => {
+        const signupName = document.querySelector('#name');
+        const signupEmail = document.querySelector('#email');
+        const signupPass = document.querySelector('#password');
+        const signupConfirmPass = document.querySelector('#confirm_password');
+
+        if (signupName.value.length != 0 && isEmail(signupEmail.value) && signupPass.value.length != 0 && signupPass.value == signupConfirmPass.value) {
+            console.log('Het werkt denk ik');
+        } else {
+            event.preventDefault();
+
+            if (signupName.value.length != 0) {
+                console.log('Er is een naam ingevuld.');
+                errorMessageName.classList.remove('errormessage');
+                signupName.classList.remove('error');
+            } else {
+                console.log('Er is geen naam ingevuld.');
+                signupName.classList.add('error');
+                errorMessageName.classList.add('errormessage');
+                errorMessageName.innerHTML = 'Er is geen naam ingevuld.';
+            }
+
+            // De else if en esle heb ik van https://codepen.io/FlorinPop17/pen/OJJKQeK
+            if (signupEmail.value.length == 0) {
+                console.log('Er is geen email ingeuld.');
+                signupEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen emailadres ingevuld.';
+            } else if (!isEmail(signupEmail.value)) {
+                console.log('Dit is geen geldig emailadres.');
+                signupEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen geldig emailadres ingevuld.';
+            } else {
+                console.log('Het emailadres is goed.');
+                errorMessageEmail.classList.remove('errormessage');
+                signupEmail.classList.remove('error');
+            }
+
+            if (signupPass.value.length != 0) {
+                if (signupPass.value == signupConfirmPass.value) {
+                    console.log('je wachwoord is hetzelfde');
+                    errorMessagePass.classList.remove('errormessage');
+                    signupPass.classList.remove('error');
+                    signupConfirmPass.classList.remove('error');
+                } else {
+                    console.log('je wachtwoord is niet gelijk');
+                    signupPass.classList.add('error');
+                    signupConfirmPass.classList.add('error');
+                    errorMessagePass.classList.add('errormessage');
+                }
+            } else {
+                console.log('wachtwoord mag niet leeg zijn');
+                errorMessagePass.classList.add('errormessage');
+                errorMessagePass.innerHTML = 'Er is geen wachtwoord ingevuld.';
+                signupPass.classList.add('error');
+                signupConfirmPass.classList.add('error');
+            }
+        }
+    });
+}
+
+// Login
+const formLogin = document.querySelector('#formlogin');
+const loginEmail = document.querySelector('#formlogin #email');
+const loginPass = document.querySelector('#formlogin #password');
+
+if (formLogin) {
+
+    // Source https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    const urlParams = new URLSearchParams(window.location.search);
+    const emailParam = urlParams.get('email');
+
+    if(emailParam) {
+        loginEmail.value = emailParam;
+    }
+
+    console.log(myParam);
+    
+    formLogin.addEventListener('submit', (event) => {
+        if (isEmail(loginEmail.value) && loginPass.value.length != 0) {
+            console.log('Volgensmij werkt het!!!');
+        } else {
+            event.preventDefault();
+
+            // De else if en esle heb ik van https://codepen.io/FlorinPop17/pen/OJJKQeK
+            if (loginEmail.value.length == 0) {
+                console.log('Er is geen email ingeuld.');
+                loginEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen emailadres ingevuld.';
+            } else if (!isEmail(loginEmail.value)) {
+                console.log('Dit is geen geldig emailadres.');
+                loginEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen geldig emailadres ingevuld.';
+            } else {
+                console.log('Het emailadres is goed.');
+                errorMessageEmail.classList.remove('errormessage');
+                loginEmail.classList.remove('error');
+            }
+
+            if (loginPass.value.length != 0) {
+                console.log('Het wachtwoord is ingevuld');
+                loginPass.classList.remove('error');
+                errorMessagePass.classList.remove('errormessage');
+            } else if (loginPass.value.length == 0) {
+                console.log('Wachtwoord mag niet leeg zijn');
+                errorMessagePass.classList.add('errormessage');
+                errorMessagePass.innerHTML = 'Er is geen wachtwoord ingevuld.';
+                loginPass.classList.add('error');
+            } else {
+                console.log('Wachtwoord is niet correct');
+                errorMessagePass.classList.add('errormessage');
+                loginPass.classList.add('error');
+            }
+        }
+    });
+}
+
 // Account
 const formAccount = document.querySelector('#formaccount');
 
 if (formAccount) {
     formAccount.addEventListener('submit', (event) => {
-        const accountName = document.querySelector('#formaccount #username');
+        const accountName = document.querySelector('#formaccount #name');
+        const accountEmail = document.querySelector('#formaccount #email');
         const accountPass = document.querySelector('#formaccount #password');
-        const errorMessageName = document.querySelector('.messagename');
-        const errorMessagePass = document.querySelector('.messagepassword');
+        const accountConfirmPass = document.querySelector('#formaccount #confirm_password');
 
-        if (accountName.value.length != 0 && isEmail(accountEmail.value) && accountPass.value.length != 0 && accountPass.value != 0) {
+        if (accountName.value.length != 0 && isEmail(accountEmail.value) && accountPass.value.length != 0 && accountPass.value == accountConfirmPass.value) {
             console.log('Het werkt denk ik');
         } else {
             event.preventDefault();
 
             if (accountName.value.length != 0) {
-                console.log('Er is een gebruikersnaam ingevuld.');
+                console.log('Er is een naam ingevuld.');
                 errorMessageName.classList.remove('errormessage');
                 accountName.classList.remove('error');
             } else {
-                console.log('Er is geen gebruikersnaam ingevuld.');
+                console.log('Er is geen naam ingevuld.');
                 accountName.classList.add('error');
                 errorMessageName.classList.add('errormessage');
-                errorMessageName.innerHTML = 'Er is geen gebruikersnaam ingevuld.';
+                errorMessageName.innerHTML = 'Er is geen naam ingevuld.';
+            }
+
+            // De else if en esle heb ik van https://codepen.io/FlorinPop17/pen/OJJKQeK
+            if (accountEmail.value.length == 0) {
+                console.log('Er is geen email ingeuld.');
+                accountEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen emailadres ingevuld.';
+            } else if (!isEmail(accountEmail.value)) {
+                console.log('Dit is geen geldig emailadres.');
+                accountEmail.classList.add('error');
+                errorMessageEmail.classList.add('errormessage');
+                errorMessageEmail.innerHTML = 'Er is geen geldig emailadres ingevuld.';
+            } else {
+                console.log('Het emailadres is goed.');
+                errorMessageEmail.classList.remove('errormessage');
+                accountEmail.classList.remove('error');
             }
 
             if (accountPass.value.length != 0) {
-                console.log('Het wachtwoord is goed');
-                accountPass.classList.remove('error');
-                accountConfirmPass.classList.remove('error');
-                errorMessagePass.classList.remove('errormessage');
+                if (accountPass.value == accountConfirmPass.value) {
+                    console.log('je wachwoord is hetzelfde');
+                    errorMessagePass.classList.remove('errormessage');
+                    accountPass.classList.remove('error');
+                    accountConfirmPass.classList.remove('error');
+                } else {
+                    console.log('je wachtwoord is niet gelijk');
+                    accountPass.classList.add('error');
+                    accountConfirmPass.classList.add('error');
+                    errorMessagePass.classList.add('errormessage');
+                }
             } else {
-                console.log('Het wachtwoord mag niet leeg zijn');
+                console.log('wachtwoord mag niet leeg zijn');
                 errorMessagePass.classList.add('errormessage');
                 errorMessagePass.innerHTML = 'Er is geen wachtwoord ingevuld.';
                 accountPass.classList.add('error');
